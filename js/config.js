b147b94a97c000666f37ec5409d64ebc75adbbb0
bannguyen <ban.nuce@gmail.com>
2025-12-30 09:06:29 +0700

    Initial commit for frontend project

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// config.js
+// Thay đường dẫn bên dưới bằng Link Render của bạn (đã làm ở bài trước)
+const API_URL = "https://wd-backend-api.onrender.com";
+
+// Xuất biến này ra (nếu dùng module) hoặc để global
+// Với HTML thuần, chỉ cần nhúng file này lên trước các file JS khác là dùng được biến API_URL
